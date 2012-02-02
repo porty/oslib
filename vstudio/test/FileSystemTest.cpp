0bec4c994508b4da646d0b18423ee17c740b76d0
Shorty <pppppppman@gmail.com>
2012-02-02 19:17:46 +1000

    Added more to file system testing

--- vstudio/test/FileSystemTest.cpp
+++ vstudio/test/FileSystemTest.cpp
@@ -6,16 +6,49 @@
 
 using namespace OS;
 
-void testA()
+void testExist()
 {
-	std::cout << "main.cpp file? " << (FileSystem::fileExists("main.cpp") ? "true" : "false") << std::endl <<
-		"main.cpp dir? " << (FileSystem::directoryExists("main.cpp") ? "true" : "false") << std::endl;
-	std::cout << "Debug file? " << (FileSystem::fileExists("Debug") ? "true" : "false") << std::endl <<
-		"Debug dir? " << (FileSystem::directoryExists("Debug") ? "true" : "false") << std::endl;
+	// touch testfile1.a
+	// touch testfile2.a
+	// touch testfile3.b
+	// touch testfile4.b
+	// touch testfile5
+	// mkdir testdir1
+
+	// file exists or not
+	if (!FileSystem::fileExists("testfile1.a"))
+	{
+		throw std::exception("Can't find file testfile1.a");
+	}
+
+	if (FileSystem::fileExists("fakefile.a"))
+	{
+		throw std::exception("Found non-existant file fakefile.a");
+	}
+
+	// dir exists or not
+	if (!FileSystem::directoryExists("testdir1"))
+	{
+		throw std::exception("Can't find dir");
+	}
+	if (FileSystem::directoryExists("fakeDir1"))
+	{
+		throw std::exception("Found non-existant dir");
+	}
+
+	// don't get confused between files and dirs
+	if (FileSystem::fileExists("testdir1"))
+	{
+		throw std::exception("Found dir as file");
+	}
+	if (FileSystem::directoryExists("testfile1.a"))
+	{
+		throw std::exception("Found file as dir");
+	}
+	std::cout << "testExist() OK" << std::endl;
 }
 
 void testFileSystem()
 {
-	testA();
-	std::cout << FileSystem::getCurrentDirectory() << std::endl;
+	testExist();
 }
