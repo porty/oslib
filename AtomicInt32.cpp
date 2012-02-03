e4129942cffa22622e25db2d71f6e71af0a003f0
Shorty <pppppppman@gmail.com>
2012-02-03 22:07:11 +1000

    AtomicInt32 implementation for Linux

--- AtomicInt32.cpp
+++ AtomicInt32.cpp
@@ -5,7 +5,7 @@ using namespace OS;
 #ifdef _WIN32
 #include <Windows.h>
 #else
-TODO: atomic int 32 includes
+#include <atomic_ops.h>
 #endif
 
 unsigned long AtomicInt32::operator ++()
@@ -13,7 +13,7 @@ unsigned long AtomicInt32::operator ++()
 #ifdef _WIN32
 	return InterlockedIncrement(&value);
 #else
-	TODO: atomic int 32 increment
+	return AO_fetch_and_add1(&value);
 #endif
 }
 
@@ -22,7 +22,7 @@ unsigned long AtomicInt32::operator --()
 #ifdef _WIN32
 	return InterlockedDecrement(&value);
 #else
-	TODO: atomic int 32 decrement
+	return AO_fetch_and_sub1(&value);
 #endif
 }
 
